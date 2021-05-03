3723a75180459d6fee53e0491890e2359c45eb29
Fachrul_Faathirullah <lenpuchongloi93@gmail.com>
2021-05-04 02:53:27 +0700

    selesai portfolio

--- src/utils/resumeData.js
+++ src/utils/resumeData.js
@@ -3,6 +3,8 @@ import GitHubIcon from "@material-ui/icons/GitHub";
 import WebOutlinedIcon from "@material-ui/icons/WebOutlined";
 import SmartphoneOutlinedIcon from "@material-ui/icons/SmartphoneOutlined";
 import AssignmentOutlinedIcon from "@material-ui/icons/AssignmentOutlined";
+import AndroidIcon from "@material-ui/icons/Android";
+import YouTubeIcon from "@material-ui/icons/YouTube";
 
 export default {
   name: "Fachrul Faathirullah",
@@ -12,7 +14,7 @@ export default {
   adress:
     "Komplek BBA Blok C No. 54 RT. 03 RW. 19, JL Komplek BBA, Cijeruk, Bojongsari, Kec. Bojongsoang, Bandung",
   about:
-    "Lahir di Bandung, 9 Mei 1999. Semenjak kecil saya memang sudah terbisa berada di depan computer, saat kecil dulu sering mengikuti lomba yang berasal dari hobi , diantaranya yaitu : lomba membaca puisi saat sd, lomba story telling saat smp , dan lomba basketball saat smk, dan secara konsisten saya selalu mejadi juara ke 2 di setiap lomba tersebut, hehe, mulai bersekolah di salah satu smk di bandung dengan jurusan TKJ saya sering membongkar pasang laptop dan pc juga memperbaiki settingan router. menurut saya yang paling susah dari ketiga itu yaitu bongkar pasang laptop, karena tidak dipelajari di sekolah dan tata letak dari setiap laptop yang berbeda, \n \n Tahun 2018 mulai ber-kuliah di Telkom University jurusan D3RPLA, jurusan ini memang saya incar dari dulu, karena memang saya aslinya berkepribadian introvert jadi ingin mengubah kekurangan menjadi kekuatan, hehe, dan alhamdulillah saya mendapat beasiswa dari tempat kuliah tersebut, tahun pertama merupakan tahun tersulit karena saya berkuliah di jurusan IT tapi tidak memiliki laptop, syukur saya berhasil  melewati masa-masa sulit tersebut, tahun kedua adalah tahun dimana saya aktif berorganisasi dan kepanitian , ikut di 3 organisasi sekaligus yaitu BEM , BPM , dan menjadi assisten lab, saya yang memang sebelumnya tidak pernah ikut ekskul atau kepanitian sangat terkejut melihat kenyataan dalam berorganisasi, saya mendapat banyak pengetahuan dan pengalaman dari berorganisasi tersebut, tahun ketiga adalah tahun dimana saya meningkatkan hard skill , yang pada implementasinya saya mulai mencoba mengikuti kursus javascript development, agile design and thingking, mobile react native  dari coursera, kotlin mobile development dari dicoding , lalu mengikuti Telkom digital Talent Incubator 2020 , yang mana disana saya belajar tentang React native Development juga belajar kolaborasi secara real dalam sebuah project",
+    "Lahir di Bandung, 9 Mei 1999. Semenjak kecil saya memang sudah terbisa berada di depan computer, saat kecil dulu sering mengikuti lomba yang berasal dari hobi , diantaranya yaitu : lomba membaca puisi saat sd, lomba story telling saat smp , dan lomba basketball saat smk, dan secara konsisten saya selalu mejadi juara ke 2 di setiap lomba tersebut, hehe, mulai bersekolah di salah satu smk di bandung dengan jurusan TKJ saya sering membongkar pasang laptop dan pc juga memperbaiki settingan router. menurut saya yang paling susah dari ketiga itu yaitu bongkar pasang laptop, karena tidak dipelajari di sekolah dan tata letak dari setiap laptop yang berbeda, \n \n Tahun 2018 mulai ber-kuliah di Telkom University jurusan D3RPLA, jurusan ini memang saya incar dari dulu, karena memang saya aslinya berkepribadian introvert jadi ingin mengubah kekurangan menjadi kekuatan, hehe, dan alhamdulillah saya mendapat beasiswa dari tempat kuliah tersebut, tahun pertama merupakan tahun beradaptasi dengan lingkungan baru , syukur saya berhasil  melewati masa-masa sulit tersebut, tahun kedua adalah tahun dimana saya aktif berorganisasi dan kepanitian , ikut di 3 organisasi sekaligus yaitu BEM , BPM , dan menjadi assisten lab, saya yang memang sebelumnya tidak pernah ikut ekskul atau kepanitian sangat terkejut melihat kenyataan dalam berorganisasi, saya mendapat banyak pengetahuan dan pengalaman dari berorganisasi tersebut, tahun ketiga adalah tahun dimana saya meningkatkan hard skill , yang pada implementasinya saya mulai mencoba mengikuti kursus javascript development, agile design and thingking, mobile react native  dari coursera, kotlin mobile development dari dicoding , lalu mengikuti Telkom digital Talent Incubator 2020 , yang mana disana saya belajar tentang React native Development juga belajar kolaborasi secara real dalam sebuah project",
   experiences: [
     {
       title: "Dkantin",
@@ -127,6 +129,54 @@ export default {
     {
       title: "Back End",
       description: ["Php", "Code Igniter", "Laravel"],
+    },
+  ],
+  projects: [
+    {
+      tag: "React_Native",
+      image:
+        "https://firebasestorage.googleapis.com/v0/b/simple-chat-app-c1480.appspot.com/o/travelme.png?alt=media&token=eea60d2c-8eae-4420-9df8-8554c73ee1ae",
+      title: "TravelMe",
+      caption: "a shot Description",
+      description: "This is my project description. Please fill it in",
+      links: [
+        {
+          link:
+            "https://play.google.com/store/apps/details?id=com.travelmeproj",
+          icon: <AndroidIcon />,
+        },
+        {
+          link: "https://www.youtube.com/watch?v=9zrDIa6t6_Q",
+          icon: <YouTubeIcon />,
+        },
+        {
+          link: "https://github.com/ffaathirullah/TravelMe",
+          icon: <GitHubIcon />,
+        },
+      ],
+    },
+    {
+      tag: "Kotlin",
+      image:
+        "https://firebasestorage.googleapis.com/v0/b/simple-chat-app-c1480.appspot.com/o/tictactoe.png?alt=media&token=4634e934-9776-4137-91a8-1d64f605a60a",
+      title: "Tic tac Toe",
+      caption: "a shot Description",
+      description: "This is my project description. Please fill it in",
+      links: [
+        {
+          link:
+            "https://play.google.com/store/apps/details?id=org.d3ifcool.tic_ta_toe",
+          icon: <AndroidIcon />,
+        },
+        {
+          link: "https://www.youtube.com/watch?v=4UcsrUQDWGg",
+          icon: <YouTubeIcon />,
+        },
+        {
+          link: "https://github.com/ffaathirullah/Kotlin_TicTactoe",
+          icon: <GitHubIcon />,
+        },
+      ],
     },
   ],
 };
